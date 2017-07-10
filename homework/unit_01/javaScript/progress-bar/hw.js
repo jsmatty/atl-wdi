aef026b04431fc052fee24c3cea2b9100bbef648
jsmatty <jscot.matty@gmail.com>
2017-07-10 08:10:36 -0400

    counter-factory hw

--- homework/unit_01/javaScript/progress-bar/hw.js
+++ homework/unit_01/javaScript/progress-bar/hw.js
@@ -3,16 +3,35 @@
 // See LICENSE for details.
 
 var timerUI = {
-  drawNumericDisplay: function(timerValue){
-    // Your Code Here
-  },
-  drawProgressBars: function(timerValue){
-    // Your Code Here
-  },
-  drawLitFuses: function(timerValue){
-    // Your Code Here
-  },
-  drawCrawlers: function(timerValue){
-    // Your Code Here
-  }
+    function(timerValue) {
+        console.log(timerValue);
+        timerVal -= 1;
+        setTimeout(countDown, 1000);
+    }
+};
+countDown();
+};
+
+var numericDisplay = document.getElementById('numeric-display');
+numericDisplay.textContent = timerValue;
+},
+var seconds = math.floor((distance % 1000 * 60) / 1000);
+drawProgressBars: function(timerValue) {
+
+    var timeElapsed = 100 - timerValue;
+    document.getElementsByClassName('progress-bar')[0].style.width = timeElapsed + '%';
+
+    var progress = 100 - timerValue;
+    var bars = document.getElementsByClassName('progress-bar');
+    for (var i = 0; i < bars.length; i++) {
+        bars[i].style.width = progress + '%';
+
+    },
+
+    drawLitFuses: function(timerValue) {
+            // Your Code Here
+        },
+        drawCrawlers: function(timerValue) {
+            // Your Code Here
+        }
 };
