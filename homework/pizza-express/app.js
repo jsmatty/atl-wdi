0036f9725c9de767371bf98b5fef00609934c0dd
jsmatty <jscot.matty@gmail.com>
2017-07-20 07:38:54 -0400

    set up hbs views

--- homework/pizza-express/app.js
+++ homework/pizza-express/app.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const hbs = require('hbs');
 const app = express();
 
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+const index = require('./');
+app.use('/', index);
+
+const topping = require('/toppings');
+app.use('/toppings', toppings);
 
 
-const port = 3000;
-app.listen(port, function() {
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
     console.log("==========================")
     console.log('LISTENING ON PORT ' + port);
     console.log("==========================")
