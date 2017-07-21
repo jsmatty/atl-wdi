6b405a568a8a2572061fa6d978c9be410f7d1b87
jsmatty <jscot.matty@gmail.com>
2017-07-21 00:54:15 -0400

    pirate homework

--- homework/unit_02/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
+++ homework/unit_02/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
@@ -1,1 +1,38 @@
-//your code below
+//requirements
+var express = require("express");
+var router = express.Router();
+var pirates = require('../models/pirates.js');
+
+//all routes for /pirate
+router.get('/', (req, res) => {
+    res.render('pirates/index', {
+        pirates: pirates
+    });
+});
+router.get('/new', (req, res) => {
+    res.render('pirates/new');
+})
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const pirate = pirates[id]
+    res.render('pirates/show', {
+        pirate: pirates
+    });
+});
+
+router.post('/index', (req, res) => {
+    const newPirate = {
+        name: req.body.name,
+        birthplace: req.body.birthplace,
+        death_year: req.body.death_year,
+        base: req.body.base,
+        nickname: req.body.nickname,
+    };
+    data.push(newPirate);
+
+    res.redirect("/pirates");
+});
+
+//exports
+module.exports = router;
--- homework/unit_02/express/express-pirates-create-read/pirates-wiki/server.js
+++ homework/unit_02/express/express-pirates-create-read/pirates-wiki/server.js
@@ -1,1 +1,47 @@
-//your code here
+var path = require('path');
+var logger = require('morgan');
+var express = require('express');
+var hbs = require('hbs');
+const bodyParser = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+var pirateController = require('./controller/pirates');
+
+app.use(logger('dev'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.set("view engine", "hbs");
+
+
+var pirateController = require('./controllers/pirates.js');
+app.use("/pirates", pirateController);
+
+app.get('/', function(req, res) {
+    res.send("pirates/index.hbs", {
+        pirates: pirates
+    });
+});
+app.get('/new', function(req, res) {
+    res.render("pirates/new.hbs");
+})
+app.post('/show', function(req, res) {
+    var newPirate = [{
+        name: 'Anne Bonny',
+        birthplace: 'Kinsale, Ireland',
+        death_year: 1782,
+        base: "Caribbean",
+        nickname: "none"
+    }];
+
+    res.render('pirates/index', {
+        pirates: newPirates
+    });
+});
+app.listen(3000, function(req, res) {
+    console.log(`Im listening on ${port}`);
+});
