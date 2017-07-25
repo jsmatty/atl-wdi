e260e368d942cc3a4d4ec7a58d7cc8786f19429f
jsmatty <jscot.matty@gmail.com>
2017-07-24 21:32:18 -0400

    in class work

--- labs/unit_02/javaScript/promises/js-promises-lab-starter/readFile.js
+++ labs/unit_02/javaScript/promises/js-promises-lab-starter/readFile.js
@@ -1,22 +1,46 @@
+//using strict mode
 'use strict';
-
+// it allows node ro parse a file and its text vales
 const fs = require('fs');
-
+console.log(process.argv)
+    //gisves us the argument we are writing
 let inFile = process.argv[2];
 
-fs.readFile(inFile, { encoding: 'utf8' }, function(error, content) {
-  if (error) {
-    console.error(error);
-  }
 
-  // 'Billy\nJames\nNick\n' --> ['Billy', 'James', 'Nick']
 
-  let lines = content.split('\n');
-
-  // clean up the array by removing the empty line
-  lines.pop();
-
-  lines.forEach(function(line) {
-    console.log('Hello, ' + line + '!');
-  });
+let readTheFilePromise = new Promise(function(resolve, reject) {
+    fs.readFile(inFile, { encoding: 'utf8' }, function(error, content) {
+        resolve(content);
+        reject("line 28 error ", error);
+    });
 });
+//The value passed to resolve will be the argument 
+//passed to the resolution handler you register by calling the promise's then method.
+readTheFilePromise.then(function(result) {
+        return result;
+    })
+    .then(function(result) {
+        let lines = result.split('\n');
+        return lines;
+    })
+    //each (then/catch) take one argument, a function
+    .then(function(lines) {
+        lines.pop();
+        return lines;
+    })
+    .then(function(lines) {
+        lines.forEach(function(line) {
+            console.log('Hello, ' + line + '!');
+        });
+    })
+
+//the value passed to reject and the rejection handler 
+//registered by calling the promise's (or a chained promise's) 
+//catch method.
+.catcg(err => {
+    console.log("there was an error!");
+    console.log(err);
+});
+// .catch(function(error) {
+//     console.log("an error occured line 49", error);
+// });
---
+++ morning_exercises/unit_02/javaScript/forEach_exercise.js
@@ -0,0 +1,8 @@
+words = ["Ho", "Chi", "Minh", "City", "was", "once", "known", "as", "Prey", "Nokor"];
+
+words.forEach(function(n, index, arr) {
+    words[index] = words[i].toUpperCase();
+
+});
+
+console.log(words);
