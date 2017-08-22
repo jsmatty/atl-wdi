0e27bb22a5cabb4c5d47cc36c5568c2126faf735
jsmatty <jscot.matty@gmail.com>
2017-08-21 21:55:30 -0400

    5. Input/Output skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -1,4 +1,5 @@
-## Challenge 1: Booleans
+## Challenge 1: 
+
 # a = true
 # b = false
 # puts a == b
@@ -6,6 +7,7 @@
 #
 
 ## Challenge 2: Nil
+
 # fishsticks = nil
 # puts fishsticks
 # puts "#{fishsticks} + was here"
@@ -13,6 +15,7 @@
 # puts fishsticks.class
 
 ## Challenge 3: Integers
+
 # d = 13
 # puts d
 # e = 3.14
@@ -21,3 +24,25 @@
 # puts d
 
 ## Challenge 4: Strings
+
+# christine = "Hi, I'm Christine"
+# likesTo = "long walks on the beach, kinda."
+# tims = "#{christine} and I like #{likesTo}"
+# puts tims
+# g = "2"
+# gToNumber = 2
+# puts g.to_i + gToNumber
+
+## Challenge 5: Input and Output
+
+# def double(number)
+#  puts number *2
+# double(8)
+
+# puts "Hello Lovely, What is your name?"
+# name = gets.chomp
+# puts "What are your favorite activities?"
+# activities = gets.chomp
+# puts "#{name} would like to #{to_do}."
+
+## Challenge 6: Arrays
